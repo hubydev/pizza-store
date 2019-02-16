237c697d507e51bfb12989d14133acfc83ef8ecd
Hubert Langlois <Huby@Huberts-MacBook-Pro.local>
2019-02-16 12:11:40 +0400

    eslint & prettier

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { btn } from './Button.module.scss';
+import React from "react";
+import { btn } from "./Button.module.scss";
 const Button = () => {
   return (
     <>
       <button
         className={btn}
-        onClick={() => alert('I am styled with CSS Modules')}
+        onClick={() => alert("I am styled with CSS Modules")}
       >
         I am button 2 - Press Me
       </button>
